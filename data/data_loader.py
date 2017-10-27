3be0849fe1e8a62267ba8ca3239c56cc4146df3e
overshiki <le.niu@hotmail.com>
2017-10-27 18:16:39 +0800

    update

--- data/data_loader.py
+++ data/data_loader.py
@@ -9,6 +9,8 @@ for mat data, it is a dict data which contains the following keys:
 	offsets
 each of the keys contains a list
 in each list, is numpy arrays
+
+I rearranged this into a new data container: character
 '''
 
 import scipy.io
@@ -21,20 +23,48 @@ def load_mat(file):
 
 mat_dict = load_mat("./data_background.mat")
 
-for key in keys:
-	print(len(mat_dict[key]))
+# for key in keys:
+# 	print(len(mat_dict[key]))
 
 
 names = []
 for i in range(len(mat_dict['names'])):
 	names.append(str(mat_dict['names'][i].tolist()[0][0]))
 
-print(names)
+# print(names)
 
 
-draw_dir = mat_dict['drawings'][0][0]
-draw_character = draw_dir[0][0]
-draw_img = draw_character[0][0]
-print(draw_img.shape)
-draw_stroke = draw_img[0][0]
-print(draw_stroke.shape)
+# draw_dir = mat_dict['drawings'][0][0]
+# draw_character = draw_dir[0][0]
+# draw_img = draw_character[0][0]
+# print(draw_img.shape)
+# draw_stroke = draw_img[0][0]
+# print(draw_stroke.shape)
+
+
+character = {}
+for index, (name, drawings_dir, images_dir, timing_dir) in enumerate(zip(names, mat_dict['drawings'], mat_dict['images'], mat_dict['timing'])):
+
+	drawings_dir, images_dir, timing_dir = drawings_dir[0], images_dir[0], timing_dir[0]
+
+	_dir = []
+	for drawings_subdir, images_subdir, timing_subdir in zip(drawings_dir, images_dir, timing_dir):
+		drawings_subdir, images_subdir, timing_subdir = drawings_subdir[0], images_subdir[0], timing_subdir[0]
+		_subdir = []
+		for drawings_img, images_img, timing_img in zip(drawings_subdir, images_subdir, timing_subdir):
+			idrawings_img, images_img, timing_img = drawings_img[0], images_img[0], timing_img[0]
+
+			_img = {}
+			_img['data'] = images_img
+			_img['time'] = []
+			_img['strokes'] = []
+			for drawings_stroke, timing_stroke in zip(drawings_img, timing_img):
+				_img['strokes'].append(drawings_stroke[0])
+				_img['time'].append(timing_stroke[0])
+			_subdir.append(_img)
+		_dir.append(_subdir)
+
+	character[name] = _dir
+
+				
+# print(character['Greek'])
